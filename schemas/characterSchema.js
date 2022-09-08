77dadeee9fac416845758abd0c3429c2ab909f74
mhbauleo <mhbfiuba@gmail.com>
2022-09-08 18:12:30 -0300

    fix: weight can't be negative now, age can be 0 now

--- schemas/characterSchema.js
+++ schemas/characterSchema.js
@@ -2,8 +2,8 @@ const Joi = require("joi");
 
 const characterSchema = Joi.object({
   name: Joi.string().required(),
-  age: Joi.number().integer().positive().required(),
-  weight: Joi.number().required(),
+  age: Joi.number().integer().min(0).required(),
+  weight: Joi.number().min(0).required(),
   story: Joi.string().required(),
 });
 
