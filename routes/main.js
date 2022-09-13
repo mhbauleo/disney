02a282741d66d0be99a55df74918ef3987019e13
mhbauleo <mhbfiuba@gmail.com>
2022-09-13 03:12:08 -0300

    feature: Added mainpage

--- routes/main.js
+++ routes/main.js
@@ -6,11 +6,13 @@ const movieRouter = require("./movieRouter");
 const genreRouter = require("./genreRouter");
 const authRouter = require("./authRouter");
 const swaggerRouter = require("./swaggerRouter");
+const mainPage = require("./mainPage");
 
 router.use("/characters", characterRouter);
 router.use("/movies", movieRouter);
 router.use("/genres", genreRouter);
 router.use("/auth", authRouter);
 router.use("/api-docs", swaggerRouter);
+router.use("/", mainPage)
 
 module.exports = router;
