2c80407ce6b75f44734e4ece1797431e15192a35
mhbauleo <mhbfiuba@gmail.com>
2022-09-12 23:26:31 -0300

    build: genre tragedy

--- model/db/initGenres.js
+++ model/db/initGenres.js
@@ -4,12 +4,13 @@ async function initGenres() {
   try {
     const genres = await Genre.findAll();
     if (genres.length === 0) {
-      await Genre.create({ name: "comedy", image: "comedy.jpeg" });
+      await Genre.create({ name: "comedy", image: "comedy.jpg" });
       await Genre.create({ name: "action", image: "action.jpeg" });
-      await Genre.create({ name: "crime", image: "crime.jpeg" });
-      await Genre.create({ name: "musical", image: "musical.jpeg" });
+      await Genre.create({ name: "crime", image: "crime.jpg" });
+      await Genre.create({ name: "musical", image: "musical.jpg" });
       await Genre.create({ name: "drama", image: "drama.jpeg" });
-      await Genre.create({ name: "adventure", image: "adventure.jpeg" });
+      await Genre.create({ name: "adventure", image: "adventure.jpg" });
+      await Genre.create({ name: "tragedy", image: "tragedy.jpg" });
     }
   } catch (e) {
     console.log(e);
